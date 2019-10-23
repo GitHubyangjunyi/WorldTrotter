a6b7b254ec96520addac48903f8164f0490c44d6
杨俊艺 <1209032479@qq.com>
2019-10-23 20:12:01 +0800

    添加MapViewController.swift并重写viewDidLoad方法以测试延时加载时机

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by 杨俊艺 on 2019/10/23.
+//  Copyright © 2019 杨俊艺. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MapViewController loaded its view.")
+    }
+}
