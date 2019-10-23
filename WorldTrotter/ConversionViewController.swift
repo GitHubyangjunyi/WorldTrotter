430fda980809fee890cb1515ce187fe523418bc4
杨俊艺 <1209032479@qq.com>
2019-10-23 20:07:16 +0800

    添加MapViewController.swift测试延时加载时机之前

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -37,6 +37,7 @@ class ConversionViewController: UIViewController, UITextFieldDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("ConversionViewController loaded its view.")
         updateCelsiusLabel()
     }
     
