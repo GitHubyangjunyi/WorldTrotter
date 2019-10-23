ee6b88749c4ab21a4430a08817a9d9df851b3853
杨俊艺 <1209032479@qq.com>
2019-10-23 11:30:52 +0800

    实现禁止输入两个小数点

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -8,20 +8,71 @@
 
 import UIKit
 
-class ConversionViewController: UIViewController
+class ConversionViewController: UIViewController, UITextFieldDelegate
 {
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
     
-    @IBAction func fahrenHeitFieldEditingChanged(_ textField: UITextField){
-        if let text = textField.text, !text.isEmpty {
-            celsiusLabel.text = text
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()//使用闭包创建格式化对象
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()//一旦华氏度有变化则更新摄氏度标签
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>?{
+        if let fahrenheitValue = fahrenheitValue {  //如果华氏度有值更新摄氏度
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateCelsiusLabel()
+    }
+    
+    //更新摄氏度标签
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {    //如果摄氏度有值则更新摄氏度标签
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
         } else {
             celsiusLabel.text = "???"
         }
     }
     
+    //华氏度文本框内容改变时触发
+    @IBAction func fahrenHeitFieldEditingChanged(_ textField: UITextField){
+        if let text = textField.text, let value = Double(text) {//如果有文字并且能够被转化成数值则华氏度值更新为值为value的Measurement对象
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    //点击视图隐藏键盘
     @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer){
         textField.resignFirstResponder()
+    }
+    
+    //禁止输入两个小数点
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        
+        if existingTextHasDecimalSeparator != nil && replacementTextHasDecimalSeparator != nil {
+            return false
+        } else {
+            return true
+        }
     }
 }
